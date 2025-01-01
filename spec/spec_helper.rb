693cc4fe00a0d47e5ebb86ee63287e447a2a92fb
Andrew Konchin <andry.konchin@gmail.com>
2025-01-01 19:06:47 +0200

    Switch to RSpec DSL in all_spec.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+Dir['./spec/support/**/*.rb'].each do |path|
+  require path
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
