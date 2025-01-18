b90ff74c55c87cb6c951a31a74046eeb10886f65
Andrew Konchin <andry.konchin@gmail.com>
2025-01-18 22:58:19 +0200

    Add stubs for Enumerable methods

---
+++ lib/build_your_own/ruby_core_library/enumerable.rb
@@ -0,0 +1,228 @@
+module BuildYourOwn
+  module RubyCoreLibrary
+    module Enumerable
+      def all?
+        raise "Not implemented"
+      end
+
+      def any?
+        raise "Not implemented"
+      end
+
+      def chain
+        raise "Not implemented"
+      end
+
+      def chunk
+        raise "Not implemented"
+      end
+
+      def chunk_while
+        raise "Not implemented"
+      end
+
+      def compact
+        raise "Not implemented"
+      end
+
+      def count
+        raise "Not implemented"
+      end
+
+      def cycle
+        raise "Not implemented"
+      end
+
+      def drop
+        raise "Not implemented"
+      end
+
+      def drop_while
+        raise "Not implemented"
+      end
+
+      def each_cons
+        raise "Not implemented"
+      end
+
+      def each_entry
+        raise "Not implemented"
+      end
+
+      def each_slice
+        raise "Not implemented"
+      end
+
+      def each_with_index
+        raise "Not implemented"
+      end
+
+      def each_with_object
+        raise "Not implemented"
+      end
+
+      def entries
+        raise "Not implemented"
+      end
+      alias_method :to_a, :entries
+
+      def filter_map
+        raise "Not implemented"
+      end
+
+      def find_index
+        raise "Not implemented"
+      end
+
+      def find
+        raise "Not implemented"
+      end
+      alias_method :detect, :find
+
+      def first
+      end
+
+      def flat_map
+        raise "Not implemented"
+      end
+      alias_method :collect_concat, :flat_map
+
+      def grep
+        raise "Not implemented"
+      end
+
+      def grep_v
+        raise "Not implemented"
+      end
+
+      def group_by
+        raise "Not implemented"
+      end
+
+      def include?
+        raise "Not implemented"
+      end
+      alias_method :member?, :include?
+
+      def inject
+        raise "Not implemented"
+      end
+      alias_method :reduce, :inject
+
+      def lazy
+        raise "Not implemented"
+      end
+
+      def map
+        raise "Not implemented"
+      end
+      alias_method :collect, :map
+
+      def max_by
+        raise "Not implemented"
+      end
+
+      def max
+        raise "Not implemented"
+      end
+
+      def min_by
+        raise "Not implemented"
+      end
+
+      def min
+        raise "Not implemented"
+      end
+
+      def minmax_by
+        raise "Not implemented"
+      end
+
+      def minmax
+        raise "Not implemented"
+      end
+
+      def none?
+        raise "Not implemented"
+      end
+
+      def one?
+        raise "Not implemented"
+      end
+
+      def partition
+        raise "Not implemented"
+      end
+
+      def reject
+        raise "Not implemented"
+      end
+
+      def reverse_each
+        raise "Not implemented"
+      end
+
+      def select
+        raise "Not implemented"
+      end
+      alias_method :find_all, :select
+      alias_method :filter, :select
+
+      def slice_after
+        raise "Not implemented"
+      end
+
+      def slice_before
+        raise "Not implemented"
+      end
+
+      def slice_when
+        raise "Not implemented"
+      end
+
+      def slice_when
+        raise "Not implemented"
+      end
+
+      def sort_by
+        raise "Not implemented"
+      end
+
+      def sort
+        raise "Not implemented"
+      end
+
+      def sum
+        raise "Not implemented"
+      end
+
+      def take
+        raise "Not implemented"
+      end
+
+      def take_while
+        raise "Not implemented"
+      end
+
+      def tally
+        raise "Not implemented"
+      end
+
+      def to_h
+        raise "Not implemented"
+      end
+
+      def to_set
+        raise "Not implemented"
+      end
+
+      def uniq
+        raise "Not implemented"
+      end
+
+      def zip
+        raise "Not implemented"
+      end
+    end
+  end
+end
